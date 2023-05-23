6f2c6101a0cd190cfd4abddde458dd34f9f51bce
指原奈々 <tabularasa0722@gmail.com>
2023-05-23 22:22:58 +0900

    ミニオンをRectangleで作成

---
+++ MinionsView/MinionsView/MinionsViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinionsViewApp.swift
+//  MinionsView
+//
+//  Created by 指原奈々 on 2023/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MinionsViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
